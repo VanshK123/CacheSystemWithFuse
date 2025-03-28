b1835850c04b8402d345990b268ae1d1838c488c
mrsnowman10 <47115036+mrsnowman10@users.noreply.github.com>
2025-03-27 20:01:01 -0400

    Add files via upload

---
+++ fuse.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fuse.h>
+#include <fstream>
+#include <string>
+
+// open the file
+int open(string fileName) {
+
+    // if file is not created
+    
+
+}
+
+// read from file
+int read() {
+
+}
+
+// write to file
+int write() {
+
+}
+
+// make directory
+int mkdir() {
+
+}
+
+// remove directory
+int rmdir() {
+
+}
+
+int unlink() {
+
+}
+
+int rename() {
+
+}
+
+int main(int argc, char* argv[]) {
+
+}
