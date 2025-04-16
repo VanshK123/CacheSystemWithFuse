84e4d62ebf4d0bdb3d0d9f4667eef7aca3e20b64
mrsnowman10 <47115036+mrsnowman10@users.noreply.github.com>
2025-04-16 02:01:54 -0400

    Almost done with FUSE ops

--- fuse.cc
+++ fuse.cc
@@ -1,10 +1,32 @@
+#define FUSE_USE_VERSION 30
+
 #include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
 // First tutorial I have been following: https://www.cs.nmsu.edu/~pfeiffer/fuse-tutorial/
 // Second tutorial I have been following: https://www.maastaar.net/fuse/linux/filesystem/c/2016/05/21/writing-a-simple-filesystem-using-fuse/
 // I think the second tutorial is more helpful.
 
+// To compile:
+// gcc ssfs.c -o ssfs `pkg-config fuse --cflags --libs`
+// ./ssfs -f [mount point]
+
 using namespace std;
+
+// data structures used
+char directory_list[256][256];
+int currentDirectoryIndex = -1;
+
+char files_list[256][256];
+int currentFileIndex = -1;
+
+char files_content[256][256];
+int currentFileContentIndex = -1;
 
 static int getAttribute(const char* filePath, struct stat st*, struct fuse_file_info *fileInfo) {
     // stats struct contains data about the file (size, ownership, etc.)
@@ -31,44 +53,134 @@ static int getAttribute(const char* filePath, struct stat st*, struct fuse_file_
 
     return 0;
 }
-// TO DO
-static int readDirectory() {
-    return 0;
-}
-// TO DO
-static int openFile(const char* filePath, struct fuse_file_info *fileInfo) {
-    int fileDirectory = ;
-    if (fileDirectory < 0) {
-        // error
-        return -1;
-    }
-    fileInfo->fh = fileDirectory;
-    return 0;
-}
-// reads data from a file
-static int readFile(const char* filePath, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
-    // pread is part of the linux library which reads data from a file descriptor
-    int pread = pread(fileInfo->fh, buf, size, offset);
-    if (pread < 0) {
-        // error
-        return -1;
-    }
 
-    return pread;
+static int readDirectory(const char *filePath, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo) {
+	
+    // . is for the current directory
+	filler(buffer, ".", NULL, 0);
+    // .. is for the parent directory
+	filler(buffer, "..", NULL, 0);
+	
+    // if user wants to see files in root directory
+	if ( strcmp( path, "/" ) == 0 ) {
+		for (int i = 0; i <= currentDirectoryIndex; ix++) {
+			filler(buffer, directory_list[i], NULL, 0 );
+        }
+		for (int i = 0; i <= currentFileIndex; i++) {
+			filler(buffer, files_list[i], NULL, 0 );
+        }
+	}
+	
+	return 0;
 }
+
 // TO DO
-static int writeFile() {
+// static int openFile(const char* filePath, struct fuse_file_info *fileInfo) {
+
+//     // TUTORIAL 1 WAS HELPFUL FOR THIS
+//     int retstat = 0;
+//     int fd;
+//     char fpath[PATH_MAX];
+
+//     // generates the file path
+//     bb_fullpath(fpath, filePath);
+    
+//     // this is for logging the file path
+//     log_msg("bb_open(fpath\"%s\", fi=0x%08x)\n", fpath, (int) fileInfo);
+    
+//     fd = open(fpath, fileInfo->flags);
+//     if (fd < 0)
+// 	retstat = bb_error("bb_open open");
+    
+//     fileInfo->fh = fd;
+//     log_fi(fileInfo);
+    
+//     return retstat;
+
+// }
+
+// reads data from a file
+static int readFile(const char* filePath, char *buffer, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+
+	int fileIndex = getFileIndex(path);
+	
+    if (fileIndex == -1) {
+        return -1;
+    }
+	
+	char *content = files_content[fileIndex];
+	
+	memcpy(buffer, content + offset, size);
+		
+	return strlen(content) - offset;
+
+}
+
+// gets the index of a file (for writeFile function)
+int getFileIndex(const char *path) {
+
+    // eliminating the slash from the root directory
+	path++;
+	
+	for (int i = 0; i < currentDirectoryIndex; curr_idx++) {
+		if (strcmp(path, files_list[i]) == 0) {
+			return i;
+        }
+    }
+	
+	return -1;
+}
+
+// writes to a file
+static int writeFile(const char *filePath, const char *buffer, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+
+    int fileIndex = getFileIndex(filePath);
+	
+    // getFileIndex returns -1 if the file doesn't exist
+	if (fileIndex == -1) {
+		return -1;
+    }
+		
+	strcpy(files_content[fileIndex], buffer); 
+
     return 0;
 }
-// TO DO
+
 // makes directories in our file system
-static int makeDirectory() {
+static int makeDirectory(const char *filePath, mode_t mode) {
+
+    // eliminates the slash of the root directory
+    path++;
+    // increment current directory
+	currentDirectory += 1;
+    // appends new directory to list of directories
+	strcpy(directory_list[currentDirectory], filePath);
+
     return 0;
+
 }
+
 // TO DO
 // remove directories in our file system
-static int removeDirectory() {
-    return 0;
+// static int removeDirectory() {
+//     return 0;
+// }
+
+// making a new file
+static int createFile(const char *filePath, mode_t mode, dev_t rdev) {
+
+    // remove slash from root directory
+    path++;
+    // increment current file index
+    currentFileIndex += 1;
+	strcpy(files_list[currentFileIndex], filePath);
+	
+    // increment current file content index
+	currentFileContentIndex += 1;
+	strcpy(files_content[currentFileContentIndex], "");
+	
+	return 0;
+
 }
 
 static struct fuse_operations operations = {
@@ -77,11 +189,12 @@ static struct fuse_operations operations = {
     // for our implementation of FUSE.
     .getattr = getAttribute,
     .readdir = readDirectory,
-    .open = openFile,
+    //.open = openFile,
     .read = readFile,
     .write = writeFile,
     .mkdir = makeDirectory,
-    .rmdir = removeDirecotry,
+    //.rmdir = removeDirecotry,
+    .mknod = createFile,
 };
 
 int main(int argc, char* argv[]) {
