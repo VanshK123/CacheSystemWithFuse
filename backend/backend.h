ad6f32bde66619bb7f9205414f92977545e30faa
Vansh <vanshkhannamis@gmail.com>
2025-05-08 20:10:23 -0400

    everything works

--- backend/backend.h
+++ backend/backend.h
@@ -17,26 +17,20 @@ struct FileInfo {
     bool        is_directory = false;
 };
 
-
 class Backend {
 public:
     virtual ~Backend() = default;
     virtual int init(const std::string& base_url, const std::string& bearer_token = "") = 0;
-
     virtual ssize_t download(const std::string& path, char* buffer, std::size_t size, off_t offset) = 0;
-
     virtual ssize_t upload(const std::string& path, const char* buffer, std::size_t size, off_t offset) = 0;
-
     virtual int remove(const std::string& path) = 0;
 };
 
-std::unique_ptr<Backend> create_backend(const std::string& url);
+std::shared_ptr<Backend> create_backend(const std::string& url);
 
 ssize_t backend_read_range(const std::string& path, char* buf, std::size_t len, off_t off);
-
-ssize_t backend_put_range(const std::string& path, const char* buf, std::size_t len, off_t off);
-
-int backend_delete(const std::string& path);
+ssize_t backend_put_range (const std::string& path, const char* buf, std::size_t len, off_t off);
+int     backend_delete    (const std::string& path);
 
 }
 
